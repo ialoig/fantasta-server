971f8e53ee69d3d140d8c0d2881cb4da43909458
fporcu <francesco.porcu@apparound.com>
2019-09-30 15:41:36 +0200

    Modifiche API players/get

--- src/routes/apis/players/get.js
+++ src/routes/apis/players/get.js
@@ -7,34 +7,27 @@ import { Constants } from 'utils'
 const get = async ( req, res, next ) =>
 {
     var params = req.query;
-    if ( params.token && params.version )
+    
+    let players = {}
+    try
     {
-        let players = {}
-        try
-        {
-            players = await Players.getAll()
-        }
-        catch (error)
-        {
-            console.error(error)
-            res.status(400).send( RESPONSE.reject( Constants.BAD_REQUEST, Constants.BAD_REQUEST, error ) )
-        }
-
-        let version = parseInt(params.version)
-            
-        let obj = {
-            version: players.version,
-            players: players.version>version ? players.players : {},
-            updated: version>=players.version
-        }
-
-        res.json( RESPONSE.resolve(Constants.OK, obj) )
+        players = await Players.getAll()
     }
-    else
+    catch (error)
     {
-        res.status(400).send( RESPONSE.reject( Constants.BAD_REQUEST, Constants.BAD_REQUEST ) )
+        console.error(error)
+        res.status(400).send( RESPONSE.reject( Constants.BAD_REQUEST, Constants.BAD_REQUEST, error ) )
     }
 
+    let version = parseInt(params.version) || 0
+        
+    let obj = {
+        version: players.version,
+        players: players.version>=version ? players.players : {},
+        updated: version>=players.version
+    }
+
+    res.json( RESPONSE.resolve(Constants.OK, obj) )
 }
 
 export default get
