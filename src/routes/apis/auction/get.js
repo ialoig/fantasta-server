f818db297221ae5f631a514348c6d42cdedee0d7
fporcu <francesco.porcu@apparound.com>
2019-10-04 14:48:12 +0200

    Modifica api auction/get

--- src/routes/apis/auction/get.js
+++ src/routes/apis/auction/get.js
@@ -4,28 +4,19 @@ import { RESPONSE } from '@pinkal/central_utilities'
 import { AuctionConfig, League } from 'database'
 import { Constants, LeagueUtils } from 'utils'
 
-// let Token = require('../../utils/token');
-
 const get = async ( req, res, next ) =>
 {
-    let params = req.query;
-    if ( params.leagueId && params.token )
+    let user = req.header('user');
+    let leagueId = req.query.leagueId
+    
+    console.log(user);
+    
+    if ( leagueId )
     {
-        let user = null
-        try
-        {
-            user = await verifyToken( params.token )
-        }
-        catch (error)
-        {
-            console.error(error)
-            res.status(400).send( RESPONSE.reject( Constants.BAD_REQUEST, Constants.BAD_REQUEST, error ) )
-        }
-
         let league = null;
         try
         {
-            league = await League.findById( params.leagueId )
+            league = await League.findById( leagueId )
         }
         catch (error)
         {
@@ -51,48 +42,13 @@ const get = async ( req, res, next ) =>
         };
         resp.user.admin = user.id==league.administrator.id;
 
-        res.json( RESPONSE.resolve(Constants.OK, resp, params.token) );
+        res.json( RESPONSE.resolve(Constants.OK, resp) );
     }
     else
     {
         res.status(400).send( RESPONSE.reject( Constants.BAD_REQUEST, Constants.BAD_REQUEST, null ) )
     }
 
-}
-
-const verifyToken = async ( tok ) =>
-{
-    let token = null
-    let user = null
-
-    try
-    {
-        token = await Token.read( tok, req )
-    }
-    catch (error)
-    {
-        console.error(error)
-        return Promise.reject(error)
-    }
-
-    try
-    {
-        user = await database.user.findById( token.id )
-    }
-    catch (error)
-    {
-        console.error(error)
-        return Promise.reject(error)
-    }
-
-    if ( user.password && user.password==token.password )
-    {
-        return Promise.resolve(user)
-    }
-    else
-    {
-        return Promise.reject()
-    }
 }
 
 export default get
