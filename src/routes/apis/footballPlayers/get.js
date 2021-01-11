58d4c4902b5beb66db2be3087966448df4681053
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-01-11 22:53:11 +0100

    wip

--- src/routes/apis/footballPlayers/get.js
+++ src/routes/apis/footballPlayers/get.js
@@ -1,32 +1,40 @@
-
-import { FootballPlayer } from '../../../database'
-import { Constants, Response } from '../../../utils'
-
+import { FootballPlayer } from "../../../database";
+import { Constants, Response } from "../../../utils";
 
 /**
- * handle API "fantasta/footballPlayers"
+ * handle API "fantasta/footballPlayers?version=0"
  * use query param "version" to check the freshness of the footballPlayer list on the mobile
  */
-export const get = async ( req, res, next ) =>
-{
-    let footballPlayers = {}
-    try
-    {
-        footballPlayers = await FootballPlayer.get()
-    }
-    catch (error)
-    {
-        console.error(error)
-        res.status(400).send( Response.reject( Constants.BAD_REQUEST, Constants.BAD_REQUEST, error ) )
-    }
+export const get = async (req, res, next) => {
+  let footballPlayers = {};
+  try {
+    footballPlayers = await FootballPlayer.get();
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .send(
+        Response.reject(Constants.BAD_REQUEST, Constants.BAD_REQUEST, error)
+      );
+  }
 
-    let mobileVersion = parseInt(req.query.version) || 0
+  let response_obj = {};
 
-    let response_obj = {
-        version: footballPlayers.version,
-        footballPlayers: footballPlayers.version===mobileVersion ? {} : footballPlayers.footballPlayers,
-        updated: mobileVersion===footballPlayers.version
-    }
+  if (footballPlayers) {
 
-    res.json( Response.resolve(Constants.OK, response_obj) )
-}
+    let mobileVersion = parseInt(req.query.version) || 0;
+
+    // if(footballPlayers.version < mobileVersion){
+    //     console.error("mobile version of footballPlayers > then current version")
+    //     res.json(Response.resolve(Constants.INT_SERV_ERR, response_obj));
+    // }
+    
+    response_obj = {
+    version: footballPlayers.version,
+    footballPlayers: footballPlayers.version === mobileVersion? {}: footballPlayers.footballPlayers,
+    updated: mobileVersion === footballPlayers.version,
+    };
+
+    res.json(Response.resolve(Constants.OK, response_obj));
+  }
+};
