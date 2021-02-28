4d6388b2cfa1f15526596933fa63fe8946a2cc08
francesco.porcu.dev <francesco.porcu.dev@outlook.it>
2021-02-28 20:09:54 +0100

    #96: Test autenticazione

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,12 +1,11 @@
 
-import "regenerator-runtime/runtime.js"
-import { request, should, use } from 'chai'
+
+import { request, use } from 'chai'
 import chaiHttp from 'chai-http'
 import config from 'config'
 
 const serverUrl = config.serverUrl || "http://localhost:3000";
 
 use(chaiHttp);
-should();
 
 export const requester = request(serverUrl)
