2aa593504ec3725d25fb5624bcb11f66155b8112
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-03-03 00:56:29 +0100

    scrape timestamp for downloading players lists

--- src/footballPlayers/index.js
+++ src/footballPlayers/index.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { read } from 'xlsx'
 import moment from 'moment'
 import fs from 'fs'
+const cheerio = require('cheerio');
 
 import { saveFootballPlayers } from './saveFootballPlayers'
 
@@ -16,17 +17,66 @@ const JobSchedule = () =>
     scheduleJob( rule, downloadPlayers )
 }
 
+const extractUrl = (str) => {
+
+    // remove spaces and new lines
+    const regex_spaces_and_newlines = /(\r\n|\n|\r|\s)/gm
+    let clean_str = str.replace(regex_spaces_and_newlines, "")
+
+    // extract url
+    // const regex_location_href = /.*location.href="\/\/([^\n\r]*)"\}\);/m
+    const regex_timestamp = /.*location.href=".*\=([^\n\r]*)"\}\);/m
+    const url = clean_str.match(regex_timestamp)[1]
+
+    return url
+}
+
+const extract_timestamp = async () => {
+    try
+    {
+        let html = await axios.get("https://www.fantacalcio.it/quotazioni-fantacalcio")
+        console.log("=========")
+        // console.log(html)
+        console.log("=========")
+        const parsedHTML = cheerio.load(html.data);
+
+        // let class_name = ".role"
+        // console.log(parsedHTML(class_name).length);
+        // console.log(parsedHTML(class_name));
+        // console.log("===================")
+
+        let script_name = "script"
+        // console.log(parsedHTML(script_name).length);
+        // console.log(parsedHTML(script_name));
+        // console.log("===================")
+
+        const script = parsedHTML(script_name).get()[31].children[0].data
+
+        return Promise.resolve(extractUrl(script))
+
+    }
+    catch (error)
+    {
+        console.error(error)
+    }
+    return Promise.resolve(null)
+}
+
 const downloadPlayers = async () =>
 {
-    // const time = moment().subtract(2, 'day').startOf('day').toDate().getTime()
-    const time = moment('08:34:55', "hh:mm:ss").toDate().getTime()
+    console.log("downloadPlayers()")
 
-    const classicUrl = config.schedule.classicUrl + time // '1614670495000' -> ultimo timestamp buono
+    const timestamp = await extract_timestamp()
+    console.log(`timestamp = ${timestamp}`)
+
+    const classicUrl = config.schedule.classicUrl + timestamp
+    console.log(`classicUrl = ${classicUrl}`)
     const classicFilename = config.schedule.excelFilenameClassic
 
     let classicFile = await downloadList( classicUrl, classicFilename )
 
-    const mantraUrl = config.schedule.mantraUrl + time // '1614670495000' -> ultimo timestamp buono
+    const mantraUrl = config.schedule.mantraUrl + timestamp
+    console.log(`mantraUrl = ${mantraUrl}`)
     const mantraFilename = config.schedule.excelFilenameMantra
 
     let mantraFile = await downloadList( mantraUrl, mantraFilename )
@@ -57,6 +107,8 @@ const downloadList = async ( url, filename ) =>
     }
     return Promise.resolve(null)
 }
+
+
 
 JobSchedule()
 
