597e9190f6706f4880965c0643e6666f144508b5
Filippo Mascolo <mascolo.filippo@gmail.com>
2021-04-01 23:32:22 +0200

    wip

--- test/integration/auth.delete.test.js
+++ test/integration/auth.delete.test.js
@@ -25,12 +25,21 @@ let user_2 = {
     // _id            // it will added once the user is created
     email: "user2@email.com",
     password: "user2_password",
-    username: "username1",
+    username: "username2",
+    leagues: []       // it will added once the league is created
+}
+
+let user_3 = {
+    // _id            // it will added once the user is created
+    email: "user3@email.com",
+    password: "user3_password",
+    username: "username3",
     leagues: []       // it will added once the league is created
 }
 
 const token_1 = tokenUtils.Create(config.token.kid, user_1.email, user_1.password, user_1.username)
 const token_2 = tokenUtils.Create(config.token.kid, user_2.email, user_2.password, user_2.username)
+const token_3 = tokenUtils.Create(config.token.kid, user_3.email, user_3.password, user_3.username)
 
 let team_1 = {
     // _id            // it will added once the team is created
@@ -44,6 +53,15 @@ let team_1 = {
 let team_2 = {
     // _id            // it will added once the team is created
     name: "team_2",
+    footballPlayers: [],
+    budget: 100,
+    user: null,       // it will added once the user is created
+    league: null      // it will added once the league is created
+}
+
+let team_3 = {
+    // _id            // it will added once the team is created
+    name: "team_3",
     footballPlayers: [],
     budget: 100,
     user: null,       // it will added once the user is created
@@ -84,6 +102,9 @@ describe("DELETE", () => {
         let user_2_db = await User.create(user_2)
         user_2["_id"] = user_2_db._id
 
+        let user_3_db = await User.create(user_3)
+        user_3["_id"] = user_3_db._id
+
         // Create Teams
         team_1.user = user_1_db._id
         let team_1_db = await Team.create(team_1)
@@ -93,10 +114,15 @@ describe("DELETE", () => {
         let team_2_db = await Team.create(team_2)
         team_2["_id"] = team_2_db._id
 
+        team_3.user = user_3_db._id
+        let team_3_db = await Team.create(team_3)
+        team_3["_id"] = team_3_db._id
+
         // Create League
         league_1.admin = user_1_db._id
         league_1.teams.push(team_1_db)
         league_1.teams.push(team_2_db)
+        league_1.teams.push(team_3_db)
         let league_1_db = await League.create(league_1)
 
         // Update User and Team
@@ -108,6 +134,10 @@ describe("DELETE", () => {
         user_2_db.leagues.push(league_1_db._id)
         await user_2_db.save()
         await team_2_db.save()
+        team_3_db.league = league_1_db._id
+        user_3_db.leagues.push(league_1_db._id)
+        await user_3_db.save()
+        await team_3_db.save()
     })
 
     after(() => {
@@ -158,7 +188,26 @@ describe("DELETE", () => {
         expect(res.body.status).to.equal(Errors.WRONG_PASSWORD.status)
     })
 
-    // todo: delete normal user
+    it("DELETE any USER of league ", async () => {
+        const res = await requester
+            .delete(api)
+            .set('Authorization', token_3)
+            .send({ password: user_3.password })
+
+        expect(res).to.have.status(200);
+        expect(res.body).to.equal(true);
+
+        const user_3_after_removal = await User.findOne({ email: user_3.email })
+        const team_3_after_removal = await Team.findOne({ name: team_3.name })
+        const league_1_after_removal = await League.findOne({ name: league_1.name })
+
+        expect(user_3_after_removal).to.be.null
+        expect(team_3_after_removal).to.be.null
+        expect(league_1_after_removal.admin.toString()).to.equal(user_1._id.toString())
+        expect(league_1_after_removal.teams).to.have.length(2)
+        expect(league_1_after_removal.teams.includes(team_1._id)).to.equal(true);
+        expect(league_1_after_removal.teams.includes(team_2._id)).to.equal(true);
+    })
 
     it("DELETE admin USER", async () => {
         const res = await requester
@@ -177,7 +226,8 @@ describe("DELETE", () => {
         expect(team_1_after_removal).to.be.null
         expect(league_1_after_removal.admin.toString()).to.equal(user_2._id.toString())
         expect(league_1_after_removal.teams).to.have.length(1)
-        expect(league_1_after_removal.teams[0].toString()).to.equal, team_2._id.toString()
+
+        expect(league_1_after_removal.teams.includes(team_2._id)).to.equal(true);
     })
 
     it("DELETE last USER of league ", async () => {
@@ -197,5 +247,4 @@ describe("DELETE", () => {
         expect(team_2_after_removal).to.be.null
         expect(league_1_after_removal).to.be.null
     })
-
 })
