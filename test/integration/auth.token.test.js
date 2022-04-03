99213c58cd9698e22c534c801fdd4b7b29bc69a0
Filippo Mascolo <mascolo.filippo@gmail.com>
2022-04-03 23:00:18 +0200

    fix auth.token.test

--- test/integration/auth.token.test.js
+++ test/integration/auth.token.test.js
@@ -5,94 +5,85 @@ import config from "config"
 import { after, beforeEach, describe, it } from "mocha"
 import { User } from "../../src/database/index.js"
 import { Errors, tokenUtils } from "../../src/utils/index.js"
-import { requester } from "./index.js"
+import { printObject, requester } from "./index.js"
 
 use(chaiHttp)
 should()
 
+const api = "/fantasta/auth/token"
+
 describe("TOKEN", () => {
-	beforeEach((done) => {
-		User.deleteMany({}, () => {
-			done()
-		})
+	beforeEach( async () => {
+
+		// Clean DB
+		await User.deleteMany()
 	})
 
-	after(() => {
+	after( async () => {
 		requester.close()
+		
+		// Clean DB
+		await User.deleteMany()
 	})
 
-	it("Token is NULL", (done) => {
-		requester.put("/fantasta/auth/token")
-			.set("Authorization", null)
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.TOKEN_NOT_VALID.code)
-				expect(res.body.status).to.equal(Errors.TOKEN_NOT_VALID.status)
-				done()
-			})
+	it("Token is NULL", async () => {
+		const res = await requester.put(api).set("Authorization", null)
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.TOKEN_NOT_VALID.code)
+		expect(res.body.status).to.equal(Errors.TOKEN_NOT_VALID.status)
 	})
 
-	it("Token is NOT CORRECT", (done) => {
+	it("Token is NOT CORRECT", async () => {
 		let token = "wejhjksdhgkjlhdjkgrsdfjhkhsjkfdfghjkdfdghjkjfkg"
-
-		requester.put("/fantasta/auth/token")
-			.set("Authorization", token)
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.TOKEN_NOT_VALID.code)
-				expect(res.body.status).to.equal(Errors.TOKEN_NOT_VALID.status)
-				done()
-			})
+		const res = await requester.put(api).set("Authorization", token)
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.TOKEN_NOT_VALID.code)
+		expect(res.body.status).to.equal(Errors.TOKEN_NOT_VALID.status)
 	})
 
-	it("Token is CORRECT but USER NOT PRESENT", (done) => {
-		let token = tokenUtils.Create(config.token.kid, "test@test.com", "123456", "username")
-		User.deleteMany({}, () => {
-			requester.put("/fantasta/auth/token")
-				.set("Authorization", token)
-				.end((err, res) => {
-					expect(res).to.have.status(400)
-					expect(res.body).to.be.a("object")
-					expect(res.body.code).to.equal(Errors.EMAIL_NOT_FOUND.code)
-					expect(res.body.status).to.equal(Errors.EMAIL_NOT_FOUND.status)
-					done()
-				})
-		})
+	it("Token is CORRECT but USER NOT PRESENT", async () => {
+		const test_user = {
+			email: "test_1@test.com",
+			password: "123456",
+			username: "username"
+		}
+		let token = tokenUtils.Create(config.token.kid, test_user.email, test_user.password, test_user.username)
+		const res = await requester.put(api).set("Authorization", token)
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_NOT_FOUND.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_NOT_FOUND.status)
+
 	})
 
-	it("USER IS PRESENT --> Name and password are CORRECT", (done) => {
-		requester.post("/fantasta/auth/register")
-			.send({ email: "test@test.com", password: "123456" })
-			.end((err, res) => {
-
-				requester.put("/fantasta/auth/token")
-					.set("Authorization", res.body.token)
-					.end((err, res) => {
-						expect(res).to.have.status(200)
-						expect(res.body).to.be.a("object").that.is.not.empty
-						expect(res.body.user).to.be.a("object").that.is.not.empty
-						expect(res.body.token).to.be.a("string")
-						done()
-					})
-			})
+	it("USER IS PRESENT --> Name and password are CORRECT", async () => {
+		const test_user = {
+			email: "test_1@test.com",
+			password: "123456",
+			username: "username"
+		}
+		const resRegister = await requester.post("/fantasta/auth/register").send({ email: test_user.email, password: test_user.password })
+		const res = await requester.put(api).set("Authorization", resRegister.body.token)
+		expect(res).to.have.status(200)
+		expect(res.body).to.be.a("object").that.is.not.empty
+		expect(res.body.user).to.be.a("object").that.is.not.empty
+		expect(res.body.token).to.be.a("string")
 	})
 
-	it("USER IS PRESENT --> Name is CORRECT and password is INCORRECT", (done) => {
-		requester.post("/fantasta/auth/register")
-			.send({ email: "test@test.com", password: "123456" })
-			.end(() => {
-				let token = tokenUtils.Create(config.token.kid, "test@test.com", "654321", "test@test.com")
-				requester.put("/fantasta/auth/token")
-					.send({ token: token })
-					.end((err, res) => {
-						expect(res).to.have.status(400)
-						expect(res.body).to.be.a("object")
-						expect(res.body.code).to.equal(Errors.TOKEN_NOT_VALID.code)    //todo: why not PASSWORD_ERROR?
-						expect(res.body.status).to.equal(Errors.TOKEN_NOT_VALID.status) //todo: why not PASSWORD_ERROR?
-						done()
-					})
-			})
+	it("USER IS PRESENT --> Name is CORRECT and password is INCORRECT", async () => {
+		const test_user = {
+			email: "test_1@test.com",
+			password: "123456",
+			username: "username"
+		}
+		await requester.post("/fantasta/auth/register").send({ email: test_user.email, password: test_user.password })	
+		let token = tokenUtils.Create(config.token.kid, test_user.email, "654321", test_user.username)
+		const res = await requester.put(api).set("Authorization", token)
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.WRONG_PASSWORD.code)
+		expect(res.body.status).to.equal(Errors.WRONG_PASSWORD.status)
 	})
 })
