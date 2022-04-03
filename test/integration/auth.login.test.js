aec83f44c0c5a3d12cca8090f2148788f67129e0
Filippo Mascolo <mascolo.filippo@gmail.com>
2022-04-03 22:19:11 +0200

    fix auth.login.test

--- test/integration/auth.login.test.js
+++ test/integration/auth.login.test.js
@@ -8,126 +8,129 @@ import { requester } from "./index.js"
 use(chaiHttp)
 should()
 
+const api = "/fantasta/auth/login"
+
 describe("LOGIN", () => {
-	beforeEach((done) => {
-		User.deleteMany({}, () => {
-			done()
-		})
+
+	beforeEach( async () => {
+
+		// Clean DB
+		await User.deleteMany()
 	})
 
-	after(() => {
+	after( async () => {
 		requester.close()
+		
+		// Clean DB
+		await User.deleteMany()
+	})
+	it("Body is undefined", async () => {
+		const res = await requester.put(api).send(undefined)
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
 	})
 
-	it("Body is undefined", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send(undefined)
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
-				expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
-				done()
-			})
+	it("Body is empty", async () => {
+		const res = await requester.put(api).send({})
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
 	})
 
-	it("Body is empty", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send({})
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
-				expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
-				done()
-			})
+	it("Email is NULL", async () => {
+		const test_user = { 
+			email: null, 
+			password: "123456", 
+			username: "username" 
+		}
+
+		const res = await requester.put(api).send({ email: test_user.email, password: test_user.password })
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
 	})
 
-	it("Email is NULL", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send({ email: null, password: "123456" })
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
-				expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
-				done()
-			})
+	it("Email is NOT VALID", async () => {
+		const test_user = { 
+			email: "asdgsdfgdsfg", 
+			password: "123456", 
+			username: "username" 
+		}
+		const res = await requester.put(api).send({ email: test_user.email, password: test_user.password })
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
 	})
 
-	it("Email is NOT VALID", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send({ email: "asdgsdfgdsfg", password: "123456" })
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.EMAIL_ERROR.code)
-				expect(res.body.status).to.equal(Errors.EMAIL_ERROR.status)
-				done()
-			})
+	it("Password is NULL", async () => {
+		const test_user = { 
+			email: "test@test.com", 
+			password: null, 
+			username: "username" 
+		}
+		const res = await requester.put(api).send({ email: test_user.email, password: test_user.password })
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.PASSWORD_ERROR.code)
+		expect(res.body.status).to.equal(Errors.PASSWORD_ERROR.status)
 	})
 
-	it("Password is NULL", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send({ email: "test@test.com", password: null })
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.PASSWORD_ERROR.code)
-				expect(res.body.status).to.equal(Errors.PASSWORD_ERROR.status)
-				done()
-			})
+	it("Password is NOT VALID", async () => {
+		const test_user = { 
+			email: "test@test.com", 
+			password: "3452", 
+			username: "username" 
+		}
+		const res = await requester.put(api).send({ email: test_user.email, password: test_user.password })
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.PASSWORD_ERROR.code)
+		expect(res.body.status).to.equal(Errors.PASSWORD_ERROR.status)
 	})
 
-	it("Password is NOT VALID", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send({ email: "test@test.com", password: "3452" })
-			.end((err, res) => {
-				expect(res).to.have.status(400)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.PASSWORD_ERROR.code)
-				expect(res.body.status).to.equal(Errors.PASSWORD_ERROR.status)
-				done()
-			})
+	it("Email and password are CORRECT but USER NOT PRESENT", async () => {
+		const test_user = { 
+			email: "test@test.com", 
+			password: "123456", 
+			username: "username" 
+		}
+		const res = await requester.put(api).send({ email: test_user.email, password: test_user.password })
+		expect(res).to.have.status(404)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_PASSWORD_ERROR.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_PASSWORD_ERROR.status)
 	})
 
-	it("Email and password are CORRECT but USER NOT PRESENT", (done) => {
-		requester.put("/fantasta/auth/login")
-			.send({ email: "test@test.com", password: "123456" })
-			.end((err, res) => {
-				expect(res).to.have.status(404)
-				expect(res.body).to.be.a("object")
-				expect(res.body.code).to.equal(Errors.EMAIL_PASSWORD_ERROR.code)
-				expect(res.body.status).to.equal(Errors.EMAIL_PASSWORD_ERROR.status)
-				done()
-			})
+	it("USER IS PRESENT --> Email is CORRECT but password is INCORRECT", async () => {
+		const test_user = { 
+			email: "test@test.com", 
+			password: "123456", 
+			username: "username" 
+		}
+		await User.create({ email: test_user.email, password: test_user.password, username: test_user.username })
+		const res = await requester.put(api).send({ email: test_user.email, password: "654321" })
+		expect(res).to.have.status(400)
+		expect(res.body).to.be.a("object")
+		expect(res.body.code).to.equal(Errors.EMAIL_PASSWORD_ERROR.code)
+		expect(res.body.status).to.equal(Errors.EMAIL_PASSWORD_ERROR.status)
 	})
 
-	it("USER IS PRESENT --> Email is CORRECT but password is INCORRECT", (done) => {
-		User.create({ email: "test@test.com", password: "123456" }, () => {
-			requester.put("/fantasta/auth/login")
-				.send({ email: "test@test.com", password: "654321" })
-				.end((err, res) => {
-					expect(res).to.have.status(400)
-					expect(res.body).to.be.a("object")
-					expect(res.body.code).to.equal(Errors.EMAIL_PASSWORD_ERROR.code)
-					expect(res.body.status).to.equal(Errors.EMAIL_PASSWORD_ERROR.status)
-					done()
-				})
-		})
-	})
-
-	it("USER IS PRESENT 200 --> Email and password are CORRECT", (done) => {
-		User.create({ email: "test@test.com", password: "123456", username: "username" }, () => {
-			requester.put("/fantasta/auth/login")
-				.send({ email: "test@test.com", password: "123456" })
-				.end((err, res) => {
-					expect(res).to.have.status(200)
-					expect(res.body).to.be.a("object").that.is.not.empty
-					expect(res.body.user).to.be.a("object").that.is.not.empty
-					expect(res.body.token).to.be.a("string")
-					done()
-				})
-		})
+	it("USER IS PRESENT 200 --> Email and password are CORRECT", async () => {
+		const test_user = { 
+			email: "test@test.com", 
+			password: "123456", 
+			username: "username" 
+		}
+		await User.create({ email: test_user.email, password: test_user.password, username: test_user.username })
+		const res = await requester.put(api).send({ email: test_user.email, password: test_user.password })
+		expect(res).to.have.status(200)
+		expect(res.body).to.be.a("object").that.is.not.empty
+		expect(res.body.user).to.be.a("object").that.is.not.empty
+		expect(res.body.token).to.be.a("string")
 	})
 })
